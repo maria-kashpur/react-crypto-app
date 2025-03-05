2f56683030983ce5d6282403ce73b71ec088e5a8
maria-kashpur <kashpur.maria@icloud.com>
2025-03-05 16:09:20 +0300

    feat: add Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,44 @@
-import './App.css'
+import React from "react";
+import { Layout } from "antd";
+
+const { Header, Sider, Content } = Layout;
+
+const headerStyle: React.CSSProperties = {
+  textAlign: "center",
+  color: "#fff",
+  height: 60,
+  paddingInline: 48,
+  lineHeight: "64px",
+  backgroundColor: "#4096ff",
+};
+
+const contentStyle: React.CSSProperties = {
+  textAlign: "center",
+  minHeight: "calc(100vh - 60px)",
+  color: "#fff",
+  backgroundColor: "#001529",
+};
+
+const siderStyle: React.CSSProperties = {
+  textAlign: "center",
+  lineHeight: "120px",
+  color: "#fff",
+  backgroundColor: "#1677ff",
+};
+
 
 function App() {
-  return <h1>React Crypto App</h1>;
+  return (
+    <Layout>
+      <Header style={headerStyle}>Header</Header>
+      <Layout>
+        <Sider width="25%" style={siderStyle}>
+          Sider
+        </Sider>
+        <Content style={contentStyle}>Content</Content>
+      </Layout>
+    </Layout>
+  );
 }
 
-export default App
+export default App;

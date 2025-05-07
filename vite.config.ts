70468690af6742cf40f6a42c34ffa279a8081099
maria-kashpur <kashpur.maria@icloud.com>
2025-05-07 15:21:42 +0300

    fix: fix base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/react-cripto-app/",
+  base: "/react-crypto-app/",
   resolve: {
     alias: {
       "@": "/src",
